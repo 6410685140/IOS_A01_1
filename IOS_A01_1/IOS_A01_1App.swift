762ae2ebfb93cf693a5c0dd365100d113b767121
IOS <6410685140@student.tu.ac.th>
2024-01-19 14:34:40 +0700

    Initial Commit

---
+++ IOS_A01_1/IOS_A01_1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_A01_1App.swift
+//  IOS_A01_1
+//
+//  Created by นายธนภัทร สาระธรรม on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct IOS_A01_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
