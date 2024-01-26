46779c76f3406959bd5fb3fbe79067558a50b230
IOS <6410685140@student.tu.ac.th>
2024-01-26 12:55:42 +0700

    success v1.1

--- IOS_A01_1/ContentView.swift
+++ IOS_A01_1/ContentView.swift
@@ -11,37 +11,38 @@ struct ContentView: View {
     @State var game = Game()
     @State var guess = Number()
     @State var showScore = false
-    @State var miss = false
     @State var count = 0
+
     var body: some View {
         VStack {
-            
-            
             Text("Guess Number: ??? ")
-            Text(guess.HL(High: game.target.Num))
+                .foregroundColor(.blue)
+                .font(.title)
+            Text("")
             
+            Text(guess.HL(High: game.target.Num))
+                .foregroundColor(.purple)
             
             ColorSlider(value: $guess.Num, trackColor: .red)
+                .padding()
+            
             Text(guess.intString())
-            
-            
-            
+                .foregroundColor(.green)
             
             Button("Hit Me!!!") {
-                
-                
                 game.check(guess: guess)
                 count = count + 1
                 if game.score != 0 {
                     showScore = true
                 }
-                
             }
-            
+            .padding()
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .cornerRadius(10)
             
             .alert(isPresented: $showScore) {
                 Alert(
-                    
                     title: Text("Your Score"),
                     message: Text(String(count)),
                     dismissButton: .default(Text("OK")) {
@@ -49,16 +50,12 @@ struct ContentView: View {
                         game.startNewGame()
                         count = 0
                     }
-                    
                 )
-                
-                
-                
             }
-            
-            Text("R: \((game.target.Num))")
         }
         .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [.yellow, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -68,9 +65,11 @@ struct ColorSlider: View {
     var body: some View {
         HStack {
             Text("0")
+                .foregroundColor(.blue)
             Slider(value: $value)
                 .accentColor(trackColor)
             Text("20")
+                .foregroundColor(.blue)
         }
     }
 }
